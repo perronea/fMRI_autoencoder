d4577895a85046f895529643933cb31d1fef4626
perronea <perronea@exanode-7-22.local>
2020-01-10 13:51:36 -0800

    expand autoencoder arcitecture and visualize results

--- autoencoder.py
+++ autoencoder.py
@@ -26,20 +26,52 @@ from keras.datasets import mnist
 from keras.regularizers import l1
 from keras.optimizers import Adam
 
+num_parcels = 352
+
+def plot_autoencoder_outputs(autoencoder):
+    decoded_pconns = autoencoder.predict(test_data)
+
+    # number of example pconns to show
+    n = 5
+    plt.figure(figsize=(10, 4.5))
+    for i in range(n):
+        # plot original pconn
+        ax = plt.subplot(2, n, i+1)
+        plt.imshow(reconstruct_pconn(test_data[i]), cmap='hot')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.set_title('Original pconn')
+
+        # plot generated pconn
+        ax = plt.subplot(2, n, i+1+n)
+        plt.imshow(reconstruct_pconn(decoded_pconns[i]), cmap='hot')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.set_title('Reconstructed pconn')
+    plt.savefig('results.png')
+        
+
+def reconstruct_pconn(pconn_vec):
+    iu = np.triu_indices(num_parcels, 1)
+    recon_pconn = np.zeros((num_parcels, num_parcels))
+    np.fill_diagonal(recon_pconn,1)
+    recon_pconn[iu] = pconn_vec
+    recon_pconn = recon_pconn + recon_pconn.T - np.diag(np.diag(recon_pconn))
+    return(recon_pconn)
 
 def load_data():
     # Load all pconns in the data directory into a numpy array from a txt
     # All pconns are created from the Gordon parcellation containing 352 parcels (size: 352x352)
     # Get just the lower triangle of the matrix
     
-    data_dir = '/home/exacloud/lustre1/fnl_lab/projects/VAE/data/test_pconn_data'
+    data_dir = '/home/exacloud/lustre1/fnl_lab/projects/VAE/data/pconn_data'
     pconn_list = os.listdir(data_dir)
 
     # Initialize empty numpy array
     pconn_data = np.empty((len(pconn_list), 61776))
 
     # indices of the upper triangle
-    iu = np.triu_indices(352,1)
+    iu = np.triu_indices(num_parcels, 1)
     for i, pconn in enumerate(pconn_list):
         pconn_path = os.path.join(data_dir, pconn)
         pconn_mat = np.loadtxt(pconn_path)
@@ -48,7 +80,7 @@ def load_data():
 
     # load numpy data into tensorflow
 
-    split = 0.2
+    split = 0.1
     test_data = pconn_data[0:int(len(pconn_list) * split)-1]
     train_data = pconn_data[int(len(pconn_list) * split):-1]
 
@@ -61,15 +93,25 @@ def load_data():
 
 input_size = 61776
 hidden_size = 432
+hidden2_size = 48
 code_size = 12
 
 input_pconn = Input(shape=(input_size,))
 hidden_1 = Dense(hidden_size, activation='relu')(input_pconn)
-code = Dense(code_size, activation='relu')(hidden_1)
-hidden_2 = Dense(hidden_size, activation='relu')(code)
+hidden2_1 = Dense(hidden2_size, activation='relu')(hidden_1)
+code = Dense(code_size, activation='relu')(hidden2_1)
+hidden2_2 = Dense(hidden2_size, activation='relu')(code)
+hidden_2 = Dense(hidden_size, activation='relu')(hidden2_2)
 output_pconn = Dense(input_size, activation='sigmoid')(hidden_2)
 
 autoencoder = Model(input_pconn, output_pconn)
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 autoencoder.fit(train_data, train_data, epochs=10)
+
+
+plot_autoencoder_outputs(autoencoder)
+
+
+
+
 
