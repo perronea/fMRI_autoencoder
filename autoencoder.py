cf11c8333bf6220e007ca0e0b7a6c51d153c4a7b
perronea <perronea@exanode-7-16.local>
2020-01-26 21:05:00 -0800

    Add classifier

--- autoencoder.py
+++ autoencoder.py
@@ -22,13 +22,12 @@ import matplotlib.pyplot as plt
 
 from keras.models import Model
 from keras.layers import Dense, Input
-from keras.datasets import mnist
 from keras.regularizers import l1
 from keras.optimizers import Adam
 
-num_parcels = 352
+NUM_PARCELS = 352
 
-def plot_autoencoder_outputs(autoencoder):
+def plot_autoencoder_outputs(autoencoder, test_data):
     decoded_pconns = autoencoder.predict(test_data)
 
     # number of example pconns to show
@@ -52,33 +51,30 @@ def plot_autoencoder_outputs(autoencoder):
         
 
 def reconstruct_pconn(pconn_vec):
-    iu = np.triu_indices(num_parcels, 1)
-    recon_pconn = np.zeros((num_parcels, num_parcels))
+    iu = np.triu_indices(NUM_PARCELS, 1)
+    recon_pconn = np.zeros((NUM_PARCELS, NUM_PARCELS))
     np.fill_diagonal(recon_pconn,1)
     recon_pconn[iu] = pconn_vec
     recon_pconn = recon_pconn + recon_pconn.T - np.diag(np.diag(recon_pconn))
     return(recon_pconn)
 
-def load_data():
+def load_encode_data(data_dir):
     # Load all pconns in the data directory into a numpy array from a txt
     # All pconns are created from the Gordon parcellation containing 352 parcels (size: 352x352)
     # Get just the lower triangle of the matrix
     
-    data_dir = '/home/exacloud/lustre1/fnl_lab/projects/VAE/data/pconn_data'
     pconn_list = os.listdir(data_dir)
 
     # Initialize empty numpy array
     pconn_data = np.empty((len(pconn_list), 61776))
 
     # indices of the upper triangle
-    iu = np.triu_indices(num_parcels, 1)
+    iu = np.triu_indices(NUM_PARCELS, 1)
     for i, pconn in enumerate(pconn_list):
         pconn_path = os.path.join(data_dir, pconn)
         pconn_mat = np.loadtxt(pconn_path)
         pconn_tri = pconn_mat[iu]
         pconn_data[i] = pconn_tri
-
-    # load numpy data into tensorflow
 
     split = 0.1
     test_data = pconn_data[0:int(len(pconn_list) * split)-1]
@@ -88,30 +84,62 @@ def load_data():
     print(test_data.shape)
     return (train_data, test_data)
 
-(train_data, test_data) = load_data()
+
+def train_autoencoder(train_data, test_data):
+
+    input_size = 61776
+    hidden_size = 432
+    hidden2_size = 48
+    latent_size = 12
+
+    # Build encoder
+
+    input_pconn = Input(shape=(input_size,))
+    hidden_1 = Dense(hidden_size, activation='relu')(input_pconn)
+    hidden2_1 = Dense(hidden2_size, activation='relu')(hidden_1)
+    latent = Dense(latent_size, activation='relu')(hidden2_1)
+    
+    encoder = Model(input_pconn, latent, name='encoder')
+    encoder.summary()
+
+    # Build decoder
+
+    latent_inputs = Input(shape=(latent_size,), name='decoder_input')
+    hidden2_2 = Dense(hidden2_size, activation='relu')(latent_inputs)
+    hidden_2 = Dense(hidden_size, activation='relu')(hidden2_2)
+    output_pconn = Dense(input_size, activation='sigmoid')(hidden_2)
+
+    decoder = Model(latent_inputs, output_pconn, name="decoder")
+    decoder.summary()
+
+    # Build autoencoder = encoder + decoder
+    #autoencoder = Model(input_pconn, output_pconn)
+    autoencoder = Model(input_pconn, decoder(encoder(input_pconn)), name='autoencoder')
+    autoencoder.summary()
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    
+    #encoder = Model(inputs=autoencoder.input, outputs=autoencoder.get_layer('encoder').output)
+
+    autoencoder.fit(train_data, train_data, epochs=20, batch_size=2)
 
 
-input_size = 61776
-hidden_size = 432
-hidden2_size = 48
-code_size = 12
-
-input_pconn = Input(shape=(input_size,))
-hidden_1 = Dense(hidden_size, activation='relu')(input_pconn)
-hidden2_1 = Dense(hidden2_size, activation='relu')(hidden_1)
-code = Dense(code_size, activation='relu')(hidden2_1)
-hidden2_2 = Dense(hidden2_size, activation='relu')(code)
-hidden_2 = Dense(hidden_size, activation='relu')(hidden2_2)
-output_pconn = Dense(input_size, activation='sigmoid')(hidden_2)
-
-autoencoder = Model(input_pconn, output_pconn)
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-autoencoder.fit(train_data, train_data, epochs=10)
+    return (autoencoder, encoder)
 
 
-plot_autoencoder_outputs(autoencoder)
+def main():
+
+    data_dir = '/home/exacloud/lustre1/fnl_lab/projects/VAE/data/pconn_data'
+
+    (train_data, test_data) = load_encode_data(data_dir)
+
+    autoencoder, encoder = train_autoencoder(train_data, test_data)
+    
+    plot_autoencoder_outputs(autoencoder, test_data)
+
+    encoder.save('gordon_pconn_encoder.h5')
 
 
-
+if __name__ == "__main__":
+    main()
 
 
---
+++ classifier.py
@@ -0,0 +1,128 @@
+#! /usr/bin/env python3
+
+#### Steps
+
+# 1) Load parcelated connectivity matrix
+# 2) Take the lower half of the connectivity matrix
+# 3) Vectorize the pconn
+# 4) Input the vectorized pconn into an encoder consisting of a fully connected neural network with n layers
+# 5) Create a decoder
+# 6) Recreate the original pconn and visualize
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+from keras.models import load_model
+
+from keras.models import Model
+from keras.layers import Dense, Input
+from keras.regularizers import l1
+from keras.optimizers import Adam
+
+from keras.models import Sequential
+
+NUM_PARCELS = 352
+
+
+
+
+
+def reconstruct_pconn(pconn_vec):
+    iu = np.triu_indices(NUM_PARCELS, 1)
+    recon_pconn = np.zeros((NUM_PARCELS, NUM_PARCELS))
+    np.fill_diagonal(recon_pconn,1)
+    recon_pconn[iu] = pconn_vec
+    recon_pconn = recon_pconn + recon_pconn.T - np.diag(np.diag(recon_pconn))
+    return recon_pconn
+
+
+def load_and_encode(model_path, train_pconns, test_pconns):
+    encoder = load_model(model_path, compile=False)
+    coded_train_pconns = encoder.predict(train_pconns)
+    coded_test_pconns = encoder.predict(test_pconns)
+
+    return (coded_train_pconns, coded_test_pconns)
+
+def train_classifier(coded_train_pconns, train_labels, coded_test_pconns, test_labels):
+    
+    classifier = Sequential()
+    classifier.add(Dense(24, input_dim=12, activation='relu'))
+    classifier.add(Dense(12, activation='relu'))
+    classifier.add(Dense(8, activation='relu'))
+    classifier.add(Dense(1, activation='sigmoid'))
+
+    classifier.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    classifier.fit(coded_train_pconns, train_labels, epochs=20, batch_size=10)
+
+    _, accuracy = classifier.evaluate(coded_test_pconns, test_labels)
+
+    print('Accuracy: %.2f' % (accuracy*100))
+    return
+    
+
+def load_class_data():
+    # Load all pconns and assign ASD subjects to class 1 and non-ASD subjects to class 0
+
+    asd_dir = '/home/exacloud/lustre1/fnl_lab/projects/VAE/data/asd'
+    adhd_dir = '/home/exacloud/lustre1/fnl_lab/projects/VAE/data/adhd'
+
+    pconn_list = []
+    for pconn in os.listdir(asd_dir):
+        full_path = os.path.join(asd_dir, pconn)
+        pconn_list.append(full_path)
+    for pconn in os.listdir(adhd_dir):
+        full_path = os.path.join(adhd_dir, pconn)
+        pconn_list.append(full_path)
+
+    labels = np.zeros(len(pconn_list))
+    labels[0:len(os.listdir(asd_dir))] = 1
+
+    pconn_ix = list(range(0,len(pconn_list)))
+    random.shuffle(pconn_ix)
+
+    # Initialize empty numpy array
+    pconn_data = np.empty((len(pconn_list), 61776))
+
+    # indices of the upper triangle
+    iu = np.triu_indices(NUM_PARCELS, 1)
+    for i, pconn_path in enumerate(pconn_list):
+        pconn_mat = np.loadtxt(pconn_path)
+        pconn_tri = pconn_mat[iu]
+        pconn_data[i] = pconn_tri
+
+    split = 0.1
+    test_data_ix = pconn_ix[0:int(len(pconn_ix) * split)-1]
+    train_data_ix = pconn_ix[int(len(pconn_ix) * split):-1]
+
+    test_pconns = pconn_data[test_data_ix]
+    train_pconns = pconn_data[train_data_ix]
+    test_labels = labels[test_data_ix]
+    train_labels = labels[train_data_ix]
+
+    print(train_pconns.shape)
+    print(test_pconns.shape)
+    print(train_labels.shape)
+    print(test_labels.shape)
+    return (train_pconns, train_labels, test_pconns, test_labels)
+
+
+def main():
+
+
+    (train_pconns, train_labels, test_pconns, test_labels) = load_class_data()
+
+    (coded_train_pconns, coded_test_pconns) = load_and_encode('/home/exacloud/lustre1/fnl_lab/projects/VAE/code/gordon_pconn_encoder.h5', train_pconns, test_pconns)
+    print(coded_train_pconns.shape)
+    print(coded_test_pconns.shape)
+
+    train_classifier(coded_train_pconns, train_labels, coded_test_pconns, test_labels)
+
+    return
+
+    
+if __name__ == "__main__":
+    main()
+
