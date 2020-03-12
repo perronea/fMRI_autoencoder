f2ace0ba2c1695b2987c945f82192fd8f0d4ff29
perronea <perronea@metal-1-0.local>
2020-03-11 22:15:12 -0700

    Add tied weights constraint

--- autoencoder.py
+++ autoencoder.py
@@ -23,11 +23,85 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 
 from keras.models import Model
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, Layer, InputSpec
 from keras.regularizers import l1
 from keras.optimizers import Adam
+from keras import backend as K
+from keras import regularizers, activations, initializers, constraints, Sequential
 
 NUM_PARCELS = 352
+
+class DenseTied(Layer):
+    def __init__(self, units,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 tied_to=None,
+                 **kwargs):
+        self.tied_to = tied_to
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        super().__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.input_spec = InputSpec(min_ndim=2)
+        self.supports_masking = True
+                
+    def build(self, input_shape):
+        assert len(input_shape) >= 2
+        input_dim = input_shape[-1]
+
+        if self.tied_to is not None:
+            self.kernel = K.transpose(self.tied_to.kernel)
+            self._non_trainable_weights.append(self.kernel)
+        else:
+            self.kernel = self.add_weight(shape=(input_dim, self.units),
+                                          initializer=self.kernel_initializer,
+                                          name='kernel',
+                                          regularizer=self.kernel_regularizer,
+                                          constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.input_spec = InputSpec(min_ndim=2, axes={-1: input_dim})
+        self.built = True
+
+    def compute_output_shape(self, input_shape):
+        assert input_shape and len(input_shape) >= 2
+        output_shape = list(input_shape)
+        output_shape[-1] = self.units
+        return tuple(output_shape)
+
+    def call(self, inputs):
+        output = K.dot(inputs, self.kernel)
+        if self.use_bias:
+            output = K.bias_add(output, self.bias, data_format='channels_last')
+        if self.activation is not None:
+            output = self.activation(output)
+        return output
+
+
+
+
 
 def plot_autoencoder_outputs(autoencoder, test_data):
     decoded_pconns = autoencoder.predict(test_data)
@@ -140,9 +214,12 @@ def train_autoencoder(train_data, test_data):
     # Build encoder
 
     input_pconn = Input(shape=(input_size,))
-    hidden_1 = Dense(hidden_size, activation='relu')(input_pconn)
-    hidden2_1 = Dense(hidden2_size, activation='relu')(hidden_1)
-    latent = Dense(latent_size, activation='relu')(hidden2_1)
+    d1 = Dense(hidden_size, activation='relu')
+    d2 = Dense(hidden2_size, activation='relu')
+    d3 = Dense(latent_size, activation='relu')
+    hidden_1 = d1(input_pconn)
+    hidden2_1 = d2(hidden_1)
+    latent = d3(hidden2_1)
     
     encoder = Model(input_pconn, latent, name='encoder')
     encoder.summary()
@@ -150,9 +227,15 @@ def train_autoencoder(train_data, test_data):
     # Build decoder
 
     latent_inputs = Input(shape=(latent_size,), name='decoder_input')
-    hidden2_2 = Dense(hidden2_size, activation='relu')(latent_inputs)
-    hidden_2 = Dense(hidden_size, activation='relu')(hidden2_2)
-    output_pconn = Dense(input_size, activation='sigmoid')(hidden_2)
+    #hidden2_2 = Dense(hidden2_size, activation='relu')(latent_inputs)
+    #hidden_2 = Dense(hidden_size, activation='relu')(hidden2_2)
+    #output_pconn = Dense(input_size, activation='sigmoid')(hidden_2)
+    td3 = DenseTied(hidden2_size, activation='relu', tied_to=d3)
+    td2 = DenseTied(hidden_size, activation='relu', tied_to=d2)
+    td1 = DenseTied(input_size, activation='sigmoid', tied_to=d1)
+    hidden2_2 = td3(latent_inputs)
+    hidden_2 = td2(hidden2_2)
+    output_pconn = td1(hidden_2)
 
     decoder = Model(latent_inputs, output_pconn, name="decoder")
     decoder.summary()
@@ -179,11 +262,11 @@ def main():
 
     (train_data, test_data) = load_pconn_data(data_dir)
 
-    #autoencoder, encoder = train_autoencoder(train_data, test_data)
+    autoencoder, encoder = train_autoencoder(train_data, test_data)
     #autoencoder.save('gordon_pconn_autoencoder.h5')
     #encoder.save('gordon_pconn_encoder.h5')
     
-    autoencoder = load_model('/home/exacloud/lustre1/fnl_lab/projects/VAE/code/gordon_pconn_autoencoder.h5', compile=False) 
+    #autoencoder = load_model('/home/exacloud/lustre1/fnl_lab/projects/VAE/code/gordon_pconn_autoencoder.h5', compile=False) 
      
     plot_autoencoder_outputs(autoencoder, test_data)
 
